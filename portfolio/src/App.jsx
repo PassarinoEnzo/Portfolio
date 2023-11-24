3b424dfe136ea38bf0764a3ccc7da8db28c92bf6
PassarinoEnzo <enzo_passarino@hotmail.com>
2023-11-23 22:10:27 -0300

    arreglos

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Route, Routes } from "react-router-dom";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Home from "./components/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
