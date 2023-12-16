e17baef3ab6c8105c8f1e92cd8ad393ae3d280da
PassarinoEnzo <enzo_passarino@hotmail.com>
2023-12-15 22:07:11 -0300

    home

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -1,3 +1,11 @@
+import Enzo from "../../assets/Enzo.jpg";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <img src={Enzo} />
+      <h1>Hola, soy Enzo Passarino!</h1>
+      <h3>Soy un apasionado por la tecnología, </h3>
+    </div>
+  );
 }
